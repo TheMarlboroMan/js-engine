59066cfb1d91cd917286ae6e6ca289832f8fb3b9
TheMarlboroMan <marlborometal@gmail.com>
2018-05-08 17:14:06 +0200

    Working on new map format

--- src/app/room.js
+++ src/app/room.js
@@ -7,6 +7,9 @@ import {map} from './map_loader.js';
 const tile_w=16;
 const tile_h=16;
 
+const obj_type_entry=1;
+const obj_type_exit=2;
+
 //TODO: This should be a bit more complex.
 export class draw_tile {
 	constructor(_x, _y, _t) {
@@ -41,6 +44,9 @@ export class room {
 		//by get_index.
 		this.tiles={};
 		this.background=[];
+
+		this.entries=[];
+		this.exits=[];
 	}
 
 	from_map(_map) {
@@ -54,7 +60,7 @@ export class room {
 
 		let process=(_source, _fn) => {
 			_source.forEach((_item) => {
-				if(_item) {
+				if(_item.t) {
 					_fn(_item.x, _item.y, _item.t);
 				}
 			});
@@ -68,7 +74,18 @@ export class room {
 			this.background.push(new draw_tile(_x, _y, _t));
 		};
 
+		let fn_obj=(_x, _y, _t) => {
+			switch(_t) {
+				case obj_type_entry: 
+					this.create_object_entry(_x, _y); break;
+				case obj_type_exit:
+					this.create_object_exit(_x, _y); break;
+				break;
+			}
+		};
+
 		process(_map.logic, fn_tiles);
+		process(_map.objects, fn_obj);
 		process(_map.background, fn_back);
 	}
 
@@ -106,5 +123,13 @@ export class room {
 
 	get_world_size_rect() {
 		return new rect(new point_2d(0,0), this.w*tile_w, this.h*tile_h);
+	}
+
+	create_object_entry(_x, _y) {
+		//TODO
+	}
+
+	create_object_exit(_x, _y) {
+		//TODO
 	}
 }
