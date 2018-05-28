b0343a8bd42da59ef543c2d61986a5da018a3b74
TheMarlboroMan <marlborometal@gmail.com>
2018-05-28 21:45:29 +0200

    Doing player attack

--- src/app/player_attack.js
+++ src/app/player_attack.js
@@ -1,19 +1,64 @@
 "use strict"
 
+import {rect, pos_right, pos_left} from '../core/rect.js';
 import {room_object} from './room_object.js';
 
-//TODO: How do I attach this to the room.
+export class deleter {
+
+	constructor() {
+		//TODO.
+	}
+
+	collect(_deletable) {
+		//TODO: Must differentiate different objects,
+		//to delete from different reference containers.
+	}
+}
+
+//TODO: This should be attached to the room as some sort of 
+//player_attack array. 
+
 export class player_attack extends room_object {
 
-	constructor(_rect) {
+	//TODO: Should be constructed with a garbage collector.
+	constructor(_rect, _gc) {
+
+		if(!(_gc instanceof deleter)) {
+			throw new Error("player_attack must be constructed with a deleter");
+		}
+
 		super(_rect);
+		this.deleter=_gc;
+
 		this.remaining_time=0.5;
 	}
 
+	attach_to(_rect, _pos) {
+		if(!(_rect instanceof rect) {
+			throw new Error("attach_to must be invoked with a rect");
+		}
+
+		//TODO: What about the height???
+		switch(_pos) {
+			case pos_right:
+
+			break;
+			case pos_left:
+
+			break;
+			default:
+				throw new Error("Invalid position for attach_to");
+			break;
+		}
+	}
+
 	loop(_delta) {
-			//TODO: Extinguish the time.
-			//TODO: When the time is done, it will need to be deleted, you see...
-			//TODO: In order for us to actually do this, we would have to add some sort of listener pattern.
+
+		this.remaning_time-=_delta;
+
+		if(this.remaining_time <= 0.0) {
+			this.deleter.collect(this);
+		}
 	}
 
 }
